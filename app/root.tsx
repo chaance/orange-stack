2420683a0fbafeb9f6dfcaa8798d4f304ea45df4
Chance Strickland <hi@chance.dev>
2022-11-23 23:21:14 -0500

    add styles and fonts to root

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,12 @@
 import type { LinksFunction, LoaderArgs, MetaFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
+	Links,
+	LiveReload,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
 } from "@remix-run/react";
 
 import resetsStylesUrl from "~/dist/styles/resets.css";
@@ -14,34 +14,46 @@ import appStylesUrl from "~/dist/styles/app.css";
 import { getUser } from "~/session.server";
 
 export const links: LinksFunction = () => {
-  return [{ rel: "stylesheet", href: appStylesUrl }];
+	return [
+		{
+			as: "font",
+			crossOrigin: "anonymous",
+			href: "/fonts/inter-var.woff2",
+			rel: "preload",
+			type: "font/woff2",
+		},
+		{ rel: "stylesheet", href: resetsStylesUrl },
+		{ rel: "stylesheet", href: appStylesUrl },
+	];
 };
 
-export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "Remix Notes",
-  viewport: "width=device-width,initial-scale=1",
-});
+export const meta: MetaFunction = () => {
+	return {
+		charSet: "utf-8",
+		title: "Remix Notes",
+		viewport: "width=device-width,initial-scale=1",
+	};
+};
 
 export async function loader({ request }: LoaderArgs) {
-  return json({
-    user: await getUser(request),
-  });
+	return json({
+		user: await getUser(request),
+	});
 }
 
 export default function App() {
-  return (
-    <html lang="en" className="h-full">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body className="h-full">
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<Meta />
+				<Links />
+			</head>
+			<body>
+				<Outlet />
+				<ScrollRestoration />
+				<Scripts />
+				<LiveReload />
+			</body>
+		</html>
+	);
 }
